ad342ccdf5c5f5cdf06df7ab7bdf4c426a5c785b
pacheco <andrephenrique@hotmail.com>
2021-01-02 18:10:36 -0300

    styling metrics

--- frontend/src/components/Metrics/Metrics.js
+++ frontend/src/components/Metrics/Metrics.js
@@ -14,9 +14,34 @@ const Metrics = (props) => {
   return (
     <div className="metrics">
 
+      <table>
+        <tr>
+          <td className="metrics-labels">
+            Palavras mais usadas em títulos:
+          </td>
+          <td className="metrics-figures">
+            <div className="chips-group">{titleCounts}</div>
+          </td>
+        </tr>
+        <tr>
+          <td className="metrics-labels">
+            Palavras mais usadas nas descrições:
+          </td>
+          <td className="metrics-figures">
+            <div className="chips-group">{descripCounts}</div>
+          </td>
+        </tr>
+        <tr>
+          <td className="metrics-labels">
+            Tempo total para assistir:
+          </td>
+          <td className="metrics-figures">
+            <span style={{ fontSize: '20px' }}>{watchDuration}</span> <span>{parseInt(watchDuration) > 1 ? 'dias': 'dia'}</span>
+          </td>
+        </tr>
+      </table>
 
-      
-      <div className="metrics-labels">
+      {/* <div className="metrics-labels">
         <p>Palavras mais usadas em títulos:</p>
         <p>Palavras mais usadas nas descrições:</p>
         <p>Tempo total para assistir:</p>
@@ -25,7 +50,7 @@ const Metrics = (props) => {
         <div className="chips-group">{titleCounts}</div>
         <div className="chips-group">{descripCounts}</div>
         <p>{watchDuration && `${watchDuration} dia(s)` || <span> </span>}</p>
-      </div>
+      </div> */}
     </div>
   );
 }
