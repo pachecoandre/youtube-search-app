03ff43ed972c085fd2dc5eb49022bb359ec62d10
pacheco <andrephenrique@hotmail.com>
2021-01-02 22:20:21 -0300

    handle errors

--- frontend/src/components/App/App.js
+++ frontend/src/components/App/App.js
@@ -25,7 +25,7 @@ const App = () => {
 
         // week config errors
         weekConfig.forEach((config, index) => {
-            if (weekConfig.every(config => !config)) {
+            if (weekConfig.every(config => !/[0-9]{1,}/.test(config))) {
                 setErrorMessage('Por favor, informe um valor válido em pelo menos em um dia da semana')
                 setErrorLocation('weekConfig')
                 throw new Error('At least one day must be greater than zero')
@@ -37,8 +37,8 @@ const App = () => {
         // search bar errors
         if (!search) {
             setErrorMessage('Por favor, informe um termo de pesquisa')
-                setErrorLocation('searchBar')
-                throw new Error('Search field cannot be empty')
+            setErrorLocation('searchBar')
+            throw new Error('Search field cannot be empty')
         }
     }
 
@@ -49,7 +49,7 @@ const App = () => {
         /* clean errors */
         setErrorMessage('')
         setErrorLocation('')
-        
+
         setFetching(true)
         try {
             const weekConfig = [monday, tuesday, wednesday, thursday, friday, saturday, sunday]
@@ -82,35 +82,35 @@ const App = () => {
                 <div className="week-settup" >
                     <div className="week-label">
                         <label>Seg</label>
-                        <input className="week-day" value={monday} onChange={(e) => setMonday(e.target.value)} name="mon" type="text" />
+                        <input className="week-day" value={monday} onChange={(e) => setMonday(e.target.value)} name="mon" type="number" />
                     </div>
                     <div className="week-label">
                         <label>Ter</label>
-                        <input className="week-day" value={tuesday} onChange={(e) => setTuesday(e.target.value)} name="tue" type="text" />
+                        <input className="week-day" value={tuesday} onChange={(e) => setTuesday(e.target.value)} name="tue" type="number" />
                     </div>
                     <div className="week-label">
                         <label>Qua</label>
-                        <input className="week-day" value={wednesday} onChange={(e) => setWednesday(e.target.value)} name="wed" type="text" />
+                        <input className="week-day" value={wednesday} onChange={(e) => setWednesday(e.target.value)} name="wed" type="number" />
                     </div>
                     <div className="week-label">
                         <label>Qui</label>
-                        <input className="week-day" value={thursday} onChange={(e) => setThursday(e.target.value)} name="thu" type="text" />
+                        <input className="week-day" value={thursday} onChange={(e) => setThursday(e.target.value)} name="thu" type="number" />
                     </div>
                     <div className="week-label">
                         <label>Sex</label>
-                        <input className="week-day" value={friday} onChange={(e) => setFriday(e.target.value)} name="fri" type="text" />
+                        <input className="week-day" value={friday} onChange={(e) => setFriday(e.target.value)} name="fri" type="number" />
                     </div>
                     <div className="week-label">
                         <label>Sáb</label>
-                        <input className="week-day" value={saturday} onChange={(e) => setSaturday(e.target.value)} name="sat" type="text" />
+                        <input className="week-day" value={saturday} onChange={(e) => setSaturday(e.target.value)} name="sat" type="number" />
                     </div>
                     <div className="week-label">
                         <label>Dom</label>
-                        <input className="week-day" value={sunday} onChange={(e) => setSunday(e.target.value)} name="sun" type="text" />
+                        <input className="week-day" value={sunday} onChange={(e) => setSunday(e.target.value)} name="sun" type="number" />
                     </div>
                 </div>
                 {errorLocation === 'weekConfig' ? <div style={{ color: 'red', paddingTop: '10px' }}>{errorMessage}</div> : null}
-                
+
                 <div>
                     <form className="search-form" onSubmit={handleSearch}>
                         <input className="search-input" value={search} placeholder="Pesquisa" onChange={(e) => setSearch(e.target.value)} name="search-input" type="text" />
@@ -118,7 +118,7 @@ const App = () => {
                     </form>
                 </div>
                 {errorLocation === 'searchBar' ? <div style={{ color: 'red', paddingTop: '10px' }}>{errorMessage}</div> : null}
-                
+
                 {fetching ? <div><img className="loading" alt="Carregando" src={loading}></img></div> : null}
                 {videos.length > 0 ? <div>
                     <Metrics metrics={metrics} />
